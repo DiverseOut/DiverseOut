fbe3e1daebd03a3742dfeb1b34627182e0903c7c
Philip Riley <hiriley510@gmail.com>
2015-03-20 16:47:11 -0700

    Adds create company form

--- public/controllers/create_company.ctrl.js
+++ public/controllers/create_company.ctrl.js
@@ -1,5 +1,23 @@
 ourApp.controller("CreateCompanyController", ['$scope', '$http', '$route', '$cookies', '$location', function($scope, $http, $route, $cookies, $location){
 
-// http post to create company
+  $scope.createCompany = function(){
+    var credentials = {
+      name:    $('input[name=name]').val(),
+      street_num:    $('input[name=street_num]').val(),
+      street_num_line_two: $('input[name=street_num_line_two]').val(),
+      street: $('input[name=street]').val(),
+      city: $('input[name=city]').val(),
+      state: $('input[name=state]').val(),
+      country: $('input[name=country]').val(),
+      website_url: $('input[name=website_url]').val(),
+      thumbnail_url: $('input[name=thumbnail_url]').val(),
+    }
+    console.log(credentials)
+    $http({
+      method: "POST",
+      url: ''
+      params: credentials
+    })
+  }
 
   }]);
--- public/controllers/main.ctrl.js
+++ public/controllers/main.ctrl.js
@@ -1,9 +1,10 @@
 ourApp.controller("MainController", ['$scope', '$http', '$route','$cookies', '$location', function($scope, $http, $route, $cookies, $location){
 
-    console.log("COOKIE: "+$cookies.user_id)
+    // console.log("COOKIE: "+$cookies.user_id)
 
     $scope.loggedInUser = $cookies.user_id
-    // console.log($scope.loggedInUser)
+    console.log($scope.loggedInUser)
+    console.log($scope.loggedInUser)
 
     $scope.logout = function(){
         delete $cookies.user_id
