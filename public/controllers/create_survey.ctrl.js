83d56a4c869162f90d964eb384e57eebdac17fa4
Philip Riley <hiriley510@gmail.com>
2015-03-20 21:26:12 -0700

    Adds create survey controller actions

--- public/controllers/create_survey.ctrl.js
+++ public/controllers/create_survey.ctrl.js
@@ -1,7 +1,6 @@
 ourApp.controller("CreateSurveyController", ['$scope', '$http', '$route', '$cookies', '$location', '$routeParams', function($scope, $http, $route, $cookies, $location, $routeParams){
 
-  $scope.companyId = $routeParams.company_id
-  console.log($scope.companyId)
+  $scope.companyId = parseInt($routeParams.company_id)
 
   $http({
     method: "GET",
@@ -11,8 +10,28 @@ ourApp.controller("CreateSurveyController", ['$scope', '$http', '$route', '$cook
     $scope.attributeGroups = response
   })
 
+  var getVals = function(){
+    var attributeGroups = []
+    var checkboxArr = $('input[type=checkbox]:checked')
+    for (var i=0;i<checkboxArr.length;i++){
+      var value = parseInt($(checkboxArr[i]).val())
+      attributeGroups.push(value)
+    }
+    return attributeGroups
+  }
+
   $scope.createSurvey = function(){
-    console.log("hey")
+    var surveyDetails = {
+      company_id:       $scope.companyId,
+      attribute_groups: getVals()
+    }
+    $http({
+      method: 'POST',
+      url: 'http://localhost:9393/admins/'+$cookies.user_id+'/companies/'+$cookies.company_id+'/surveys',
+      params: surveyDetails
+    }).success(function(response){
+      console.log(response)
+    })
   }
 
 }]);
