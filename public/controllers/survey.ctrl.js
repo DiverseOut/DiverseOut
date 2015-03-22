ded0676b23418f14c2c79c3c242a663c8e9e085d
Philip Riley <hiriley510@gmail.com>
2015-03-22 01:01:40 -0700

    Modifies survey controller and view

--- public/controllers/survey.ctrl.js
+++ public/controllers/survey.ctrl.js
@@ -3,18 +3,46 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
   $scope.companyId = parseInt($routeParams.company_id)
   $scope.surveyId = parseInt($routeParams.survey_id)
 
-  $('.list-group-item').click(function(){
-    $(this).css("background-color","blue")
-    console.log($(this))
-  })
+  $scope.postSuccessArr = []
 
-// create multiple responses on each check
   $http({
-    method: "GET",
-    url: 'http://localhost:9393/companies/'+$scope.companyId+'/surveys/'+$scope.surveyId
+    method: 'GET',
+    url: 'http://localhost:9393/companies/'+$scope.companyId+'/surveys/'+$scope.surveyId,
   }).success(function(response){
-    console.log(response)
     $scope.surveyInfo = response
   })
+
+  var getVals = function(){
+    var attributeGroups = []
+    var selectedArr = $('option:selected')
+    for (var i=0;i<selectedArr.length;i++){
+      var value = parseInt($(selectedArr[i]).val())
+      attributeGroups.push(value)
+    }
+    postResponses(attributeGroups)
+  }
+
+// create multiple responses on each check
+
+  var postResponses = function(responseArr){
+
+    function httpPost(param){
+      $http({
+        method: 'POST',
+        url: 'http://localhost:9393/companies/'+$scope.companyId+'/responses',
+        params: param
+      }).success(function(response){
+        postSuccessArr.push(response)
+      })
+    }
+
+    for (i=0;i<responseArr.length;i++){
+      httpPost(responseArr[i])
+    }
+
+    console.log($scope.postSuccessArr)
+
+  }
+
 
 }]);
