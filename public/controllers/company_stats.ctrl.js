b8a048342b68e4683e74c77f2d89d3bcf49b3519
Philip Riley <hiriley510@gmail.com>
2015-03-25 17:26:08 -0700

    Adds company stats controller

--- public/controllers/company_stats.ctrl.js
+++ public/controllers/company_stats.ctrl.js
@@ -2,6 +2,16 @@ ourApp.controller("CompanyStatsController", ['$scope', '$http', '$route', '$cook
 
   $scope.companyId = parseInt($routeParams.company_id)
 
+  $http({
+    method: "GET",
+    url: "http://localhost:9393/companies/"+$scope.companyId+"/responses"
+  }).success(function(response){
+    console.log(response)
+    $scope.totalEmployees = response.company_total_employees
+    $scope.totalResponses = response.comapny_total_responses
+    $scope.companyStats = response.response_stats
+  })
+
 
 
 
