cef7dccc7c145723cc6abdef6402be0eb846b7fe
Philip Riley & Vincent Colavin <pair+philril.vcolavin@devbootcamp.com>
2015-04-05 15:39:14 -0700

    trying to fix bugs in survey.ctrl.js

--- public/controllers/survey.ctrl.js
+++ public/controllers/survey.ctrl.js
@@ -7,14 +7,14 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
     method: 'GET',
     url: 'http://localhost:3000/companies/'+$scope.companyId+'/surveys/'+$scope.surveyId,
   }).success(function(response){
-    console.log(response)
+    // console.log(response)
     $scope.surveyInfo = response
     // submitForm
     $scope.getVals = function(){
       // PROMISE
       ourIife = submitForm()
       ourIife.makeObjsToPost().then(ourIife.postResponses());
-      console.log("getVals yeah!")
+      // console.log("getVals yeah!")
     }
   })
 
@@ -25,28 +25,35 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
 
     // Closure values:
     var getValuesOfSelectedAttributes = function(arr){
+      // arr is not an array but a jQuery object.
+      // As such, we have to create returnArray explicitly as an array
+      returnArray = []
+
       for (var i=0;i<arr.length;i++){
-        arr[i] = parseInt($(arr[i]).val())
+        returnArray[i] = parseInt($(arr[i]).val())
       }
-      return arr
+
+      return returnArray
     };
 
     var employees = getValuesOfSelectedAttributes($('input[type=checkbox]:checked'));
     var attributes = getValuesOfSelectedAttributes($('option:selected'));
 
+    // return object
     return {
 
-      // define postResponses as $.Deferred((here's the function))
       postResponses: function(){
         var postStatusArr = []
 
         function httpPost(parameters){
-          debugger
+          // debugger
           jQuery.ajax({
-            method: 'POST',
+            type: 'POST',
             url: 'http://localhost:3000/companies/'+$scope.companyId+'/responses',
-            data: parameters
-          }).success(function(response){
+            data: parameters,
+            traditional: true
+            // processData: false
+          }).done(function(response){
             console.log("Post success!")
             postStatusArr.push(response)
           }).fail(function(error) {
@@ -60,24 +67,19 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
         function loopPosts(){
           for (i=0;i<attributes.length;i++){
             httpPost(attributes[i])
-            // console.log(i)
           }
-          // callback()
-          // debugger
-          // console.log(attributes)
         }
 
         loopPosts()
-        // Add logic here to only redirect if all posts are successful
+
+        // TODO: Add logic here to only redirect if all posts are successful
         $location.path('/company_stats/'+$scope.companyId)
       },
 
       makeObjsToPost: function(){
-        // console.log(attributes)
-
         var deferred = new $.Deferred()
-
         var objsToPost = [];
+
         for (var i=0;i<attributes.length;i++){
           objsToPost.push({
             company_id: $scope.companyId,
@@ -86,10 +88,8 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
             employee_types: employees //PROBLEM IS HERE DOGG
           })
         }
-        console.log(objsToPost)
+
         attributes = objsToPost;
-        // console.log(attributes)
-        // debugger
 
         deferred.resolve();
         return deferred.promise();
@@ -97,10 +97,5 @@ ourApp.controller("ShowSurveyController", ['$scope', '$http', '$route', '$cookie
 
     }
   };
-
-
-// Function too long! Refactor. Also, put secret key form in here!!
-
-
 
 }]);
