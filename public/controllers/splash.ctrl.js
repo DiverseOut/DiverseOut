0585d4a1e508eba8eb439935a2828ebc16dcfd1b
Philip Riley <hiriley510@gmail.com>
2015-04-10 19:16:33 -0700

    Putting company info in modals! (todo:delete separate page for each company_stats)

--- public/controllers/splash.ctrl.js
+++ public/controllers/splash.ctrl.js
@@ -1,8 +1,21 @@
 ourApp.controller("SplashController", ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location){
 
 // CHANGE TO HEROKU URL ONCE API IS LIVE:
-  $http.get("http://localhost:3000/").success(function(response){
+  $http.get(API_ROOT).success(function(response){
     $scope.companies = response
   })
+
+  $scope.getCompanyInfo = function(companyId) {
+    console.log(companyId)
+    $http({
+      method: "GET",
+      url: API_ROOT + "/companies/"+companyId+"/responses"
+    }).success(function(response){
+      console.log(response)
+      $scope.companyInfo = response.company_info
+      $scope.companyStats = response.response_stats
+      debugger
+    })
+  }
 
 }]);
